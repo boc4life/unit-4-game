8a8b6f96f9549c61cf8fb0066ed578b8255b29bb
boc4life <lheandrew@gmail.com>
2018-11-21 14:25:43 -0500

    notes added to javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    //Declare variables
 var redGem = 0;
 var yellowGem = 0;
 var greenGem = 0;
@@ -10,11 +11,13 @@ var wins = 0;
 var losses = 0;
 
 
-
+    // Function to set game state for start
 function startGame () {
+        // Set display of number of wins and losses
     $("#winsDisplay").text(wins);
     $("#lossesDisplay").text(losses);
     
+        // Obtain random number between 1-12 for each gem, give each gem an attribute with a value of that number
     redGem = Math.floor(Math.random() * 12) + 1;
     $("#redGemClick").attr("value", redGem);
 
@@ -27,17 +30,21 @@ function startGame () {
     orangeGem = Math.floor(Math.random() * 12) + 1;
     $("#orangeGemClick").attr("value", orangeGem);
 
+        // Obtain random number between 19 and 120 for target
     targetScore = Math.floor(Math.random() * 101) + 19;
     $("#targetNum").text(targetScore);
 
+        // Set running score number to 0 and update it on the display
     score = 0;
     $("#scoreDisplay").text(score);
 }
 
-$(".gem").on("click", function () {
+        // Function for when gems are clicked
+    $(".gem").on("click", function () {
     score = parseInt($(this).val()) + score;
     $("#scoreDisplay").text(score);
 
+        // Set conditionals for when the target score is matched or exceeded
     if (score == targetScore) {
         wins++;
         startGame();
@@ -50,6 +57,6 @@ $(".gem").on("click", function () {
     
 })
 
-
+        // Run startGame function on page load
 startGame();
 })
