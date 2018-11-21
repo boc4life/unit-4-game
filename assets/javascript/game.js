1d23a83b5e2b9b76b5a67fa3f45aa80d28022322
boc4life <lheandrew@gmail.com>
2018-11-20 23:46:52 -0500

    figured out end game logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var redGem = 0;
 var yellowGem = 0;
 var greenGem = 0;
 var orangeGem = 0;
-var targetScore = 0;
+var targetScore;
 var score = 0;
 var wins = 0;
 var losses = 0;
@@ -15,9 +15,6 @@ function startGame () {
     $("#winsDisplay").text(wins);
     $("#lossesDisplay").text(losses);
     
-    score = 0;
-    $("#scoreDisplay").text(score);
-
     redGem = Math.floor(Math.random() * 12) + 1;
     $("#redGemClick").attr("value", redGem);
 
@@ -32,22 +29,27 @@ function startGame () {
 
     targetScore = Math.floor(Math.random() * 101) + 19;
     $("#targetNum").text(targetScore);
+
+    score = 0;
+    $("#scoreDisplay").text(score);
 }
 
 $(".gem").on("click", function () {
     score = parseInt($(this).val()) + score;
     $("#scoreDisplay").text(score);
+
+    if (score == targetScore) {
+        wins++;
+        startGame();
+    }
+    
+    if (score > targetScore) {
+        losses++;
+        startGame();
+    }
+    
 })
 
-if (score == targetScore) {
-    wins++;
-    startGame();
-}
-
-if (score > targetScore) {
-    losses++;
-    startGame();
-}
 
 startGame();
 })
