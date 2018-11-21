9cf7f0883bdf611a1d1e9d1a31bc7e2978a9cf82
boc4life <lheandrew@gmail.com>
2018-11-20 22:30:40 -0500

    pushing to work on laptop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,8 +10,18 @@ var losses = 0;
 function startGame () {
     score = 0;
     $("totalScore").text(score);
+
     redGem = Math.floor(Math.random() * 12) + 1;
+    $("#redGemClick").attr("value", redGem);
+
     yellowGem = Math.floor(Math.random() * 12) + 1;
+    $("#yellowGemClick").attr("value", yellowGem);
+
     greenGem = Math.floor(Math.random() * 12) + 1;
+    $("#greenGemClick").attr("value", greenGem);
+
     orangeGem = Math.floor(Math.random() * 12) + 1;
+    $("#orangeGemClick").attr("value", orangeGem);
+
+    targetScore = Math.floor(Math.random() * 101) + 19;
 }
